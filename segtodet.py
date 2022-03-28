b1fdd4713d5de711482da2f2f3b5e807b884cd95
sophiahsu <sophia303v@gmail.com>
2022-03-28 18:11:54 +0800

    Create segtodet.py

---
+++ segtodet.py
@@ -0,0 +1,14 @@
+from skimage.measure import label as sk_label
+from skimage.measure import regionprops as sk_regions
+
+def segtodet(segmap, _label):
+  sk_mask = sk_label(segmap)
+  regions = sk_regions(sk_mask)
+  bboxs = []
+  for region in regions:
+    print('[INFO]bbox: ', region.bbox)
+    y1, x1, y2, x2 = region.bbox
+    bbox = {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'label': _label}
+    bboxs.append(bbox)
+  return bboxs
+    
